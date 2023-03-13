4e68ea35a40805eb021770bc5f5a27a55d151f67
GeorgeBacalu <georgebacalu83@gmail.com>
2023-03-13 11:08:24 +0200

    Initial commit

---
+++ src/main/java/com/endava/challenge/CleanCodeChallengeCourseApplication.java
@@ -0,0 +1,13 @@
+package com.endava.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCodeChallengeCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanCodeChallengeCourseApplication.class, args);
+    }
+
+}
